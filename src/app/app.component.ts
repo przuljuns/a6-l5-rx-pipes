8159771f311a398042927e8e50b8c644973233b9
djordjeprzulj <przulj@gmail.com>
2018-10-14 13:22:28 +0200

    Ispis prva tri cela broja.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
-// import { take, map } from 'rxjs/operators';
+import { take /*, map */} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,8 @@ export class AppComponent implements OnInit {
   title = 'a6-l5-rx-pipes';
 
   ngOnInit() {
-    const obs = interval(1000);
+    const obs = interval(1000)
+                .pipe(take(3));
                 // .pipe(take(3), map(v => (v + 1)));
                 // .pipe(take(3), map(v => new Date()));
     obs.subscribe(value => console.log('Subscriber ' + value));

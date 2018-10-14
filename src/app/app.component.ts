c06176983ec29630201e9b72e85d1ae10996b200
djordjeprzulj <przulj@gmail.com>
2018-10-14 12:59:50 +0200

    rx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'a6-l5-rx-pipes';
+
+  ngOnInit() {
+    let obs = interval(1000)
+                //.pipe(take(3), map(v => (v + 1)));
+                .pipe(take(3), map(v => new Date()));
+    obs.subscribe(value => console.log("Subscriber " + value));
+  }
 }

53d2408d2c09da64071f809daf8c180936a8fe78
djordjeprzulj <przulj@gmail.com>
2018-10-14 13:20:36 +0200

    Ispit celih brojeva.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
-import { take, map } from 'rxjs/operators';
+// import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a6-l5-rx-pipes';
 
   ngOnInit() {
-    let obs = interval(1000)
-                //.pipe(take(3), map(v => (v + 1)));
-                .pipe(take(3), map(v => new Date()));
-    obs.subscribe(value => console.log("Subscriber " + value));
+    const obs = interval(1000);
+                // .pipe(take(3), map(v => (v + 1)));
+                // .pipe(take(3), map(v => new Date()));
+    obs.subscribe(value => console.log('Subscriber ' + value));
   }
 }
